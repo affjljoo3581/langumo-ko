1c59f5053b9653759a09e290345c377381f1aed5
affjljoo3581 <affjljoo3581@gmail.com>
2020-09-28 23:28:03 +0900

    Fix bugs in counting korean characters

--- src/langumo_ko/utils.py
+++ src/langumo_ko/utils.py
@@ -6,4 +6,4 @@ def korean_character_ratio(text: str, ignore_whitespace: bool = True) -> float:
 
     korean_characters = len([
         c for c in text if ord('가') <= ord(c) <= ord('힣')])
-    return korean_characters / len(text)
+    return korean_characters / len(text) if len(text) > 0 else 0
