c370ec973c001c018e0d4c1477488933b0b32ebd
affjljoo3581 <affjljoo3581@gmail.com>
2020-09-28 23:08:30 +0900

    Add some utility functions

---
+++ src/langumo_ko/utils.py
@@ -0,0 +1,9 @@
+
+
+def korean_character_ratio(text: str, ignore_whitespace: bool = True) -> float:
+    if ignore_whitespace:
+        text = ''.join(text.split())
+
+    korean_characters = len([
+        c for c in text if ord('가') <= ord(c) <= ord('힣')])
+    return korean_characters / len(text)
