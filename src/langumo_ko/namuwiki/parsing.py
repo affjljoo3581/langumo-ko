0af76e7591bf45726a2fad8a13052c492c335ddf
affjljoo3581 <affjljoo3581@gmail.com>
2020-09-28 23:08:57 +0900

    Implement a langumo parser for namuwiki dataset

---
+++ src/langumo_ko/namuwiki/parsing.py
@@ -0,0 +1,25 @@
+import ijson
+from langumo.building import Parser
+from langumo.utils import AuxiliaryFile
+import langumo_ko.utils as utils
+from langumo_ko.namuwiki.rendering import NamuWikiRenderer
+from typing import Iterable
+
+
+class NamuWikiParser(Parser):
+    def extract(self, raw: AuxiliaryFile) -> Iterable[str]:
+        with raw.open('r') as fp:
+            for prefix, event, value in ijson.parse(fp):
+                if not prefix.endswith('.text'):
+                    continue
+
+                # Skip the redirection pages.
+                if value.lower().strip().startswith('#redirect'):
+                    continue
+
+                yield value
+
+    def parse(self, text: str) -> str:
+        text = NamuWikiRenderer.render(text)
+        return '\n'.join([line for line in text.splitlines()
+                          if utils.korean_character_ratio(line) > 0.1])
