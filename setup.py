b441daf949dc8e26b5a94b9bb94c588a707c5287
affjljoo3581 <affjljoo3581@gmail.com>
2020-10-06 23:25:58 +0900

    Upgrade the package version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='langumo-ko',
-    version='0.1.1',
+    version='0.1.2',
 
     author='Jungwoo Park',
     author_email='affjljoo3581@gmail.com',
