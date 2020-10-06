43611e72d2b9a4ab65040e8325aa016b30ffae08
affjljoo3581 <affjljoo3581@gmail.com>
2020-10-06 23:08:45 +0900

    Implement an abstract parser class for ModuCorpus

---
+++ src/langumo_ko/moducorpus/base.py
@@ -0,0 +1,26 @@
+import ijson
+import zipfile
+from typing import Iterable, IO
+from langumo.building import Parser
+from langumo.utils import AuxiliaryFile
+
+
+class ModuBaseParser(Parser):
+    def _extract_json_file(self, fp: IO[str]) -> Iterable[str]:
+        paragraph = []
+
+        for prefix, event, value in ijson.parse(fp):
+            if prefix == 'document.item.paragraph.item.form':
+                paragraph.append(value)
+            elif prefix == 'document.item.paragraph' and event == 'end_array':
+                yield '\n'.join(paragraph)
+                paragraph.clear()
+
+    def extract(self, raw: AuxiliaryFile) -> Iterable[str]:
+        with zipfile.ZipFile(raw.name, 'r') as zfp:
+            for filename in zfp.namelist():
+                if filename.startswith('#'):
+                    continue
+
+                with zfp.open(filename, 'r') as fp:
+                    yield from self._extract_json_file(fp)
