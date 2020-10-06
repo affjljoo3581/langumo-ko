82dd600842c167dc4417196ab5350e4ccc9c7482
affjljoo3581 <affjljoo3581@gmail.com>
2020-10-06 23:09:31 +0900

    Change the way for parsing namuwiki

--- src/langumo_ko/namuwiki/parsing.py
+++ src/langumo_ko/namuwiki/parsing.py
@@ -1,9 +1,9 @@
 import ijson
+from typing import Iterable
 from langumo.building import Parser
 from langumo.utils import AuxiliaryFile
 import langumo_ko.utils as utils
 from langumo_ko.namuwiki.rendering import NamuWikiRenderer
-from typing import Iterable
 
 
 class NamuWikiParser(Parser):
@@ -22,4 +22,5 @@ class NamuWikiParser(Parser):
     def parse(self, text: str) -> str:
         text = NamuWikiRenderer.render(text)
         return '\n'.join([line for line in text.splitlines()
-                          if utils.korean_character_ratio(line) > 0.1])
+                          if utils.korean_character_ratio(line) > 0.1
+                          and '.' in line])
