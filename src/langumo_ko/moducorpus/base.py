a5d05b1ed8725c4b7cbe784975ca6493bb1beca3
affjljoo3581 <affjljoo3581@gmail.com>
2020-10-06 23:49:03 +0900

    Change to replace abnormal quotes

--- src/langumo_ko/moducorpus/base.py
+++ src/langumo_ko/moducorpus/base.py
@@ -1,4 +1,5 @@
 import os
+import re
 import ijson
 import zipfile
 from typing import Iterable, IO
@@ -7,6 +8,9 @@ from langumo.utils import AuxiliaryFile
 
 
 class ModuBaseParser(Parser):
+    single_quotes_pattern = re.compile('[\x60\xb4\u2018\u2019]')
+    double_quotes_pattern = re.compile('[\u201c\u201d]')
+
     def _extract_json_file(self, fp: IO[str]) -> Iterable[str]:
         paragraph = []
 
@@ -25,3 +29,23 @@ class ModuBaseParser(Parser):
 
                 with zfp.open(filename, 'r') as fp:
                     yield from self._extract_json_file(fp)
+
+    def parse(self, text: str) -> str:
+        filtered = []
+        for line in text.strip().splitlines():
+            if not line:
+                continue
+
+            # Remove duplicated spaces.
+            line = line.replace('\n', ' ').replace('\t', ' ')
+            while '  ' in line:
+                line = line.replace('  ', ' ')
+
+            # Normalize the quotes by replacing unusual ones to the standard
+            # ones.
+            line = ModuBaseParser.single_quotes_pattern.sub('\'', line)
+            line = ModuBaseParser.double_quotes_pattern.sub('"', line)
+
+            filtered.append(line)
+
+        return '\n'.join(filtered)
--- src/langumo_ko/namuwiki/parsing.py
+++ src/langumo_ko/namuwiki/parsing.py
@@ -7,6 +7,9 @@ from langumo_ko.namuwiki.rendering import NamuWikiRenderer
 
 
 class NamuWikiParser(Parser):
+    single_quotes_pattern = re.compile('[\x60\xb4\u2018\u2019]')
+    double_quotes_pattern = re.compile('[\u201c\u201d]')
+
     def extract(self, raw: AuxiliaryFile) -> Iterable[str]:
         with raw.open('r') as fp:
             for prefix, event, value in ijson.parse(fp):
@@ -20,7 +23,23 @@ class NamuWikiParser(Parser):
                 yield value
 
     def parse(self, text: str) -> str:
-        text = NamuWikiRenderer.render(text)
-        return '\n'.join([line for line in text.splitlines()
-                          if utils.korean_character_ratio(line) > 0.1
-                          and '.' in line])
+        filtered = []
+        for line in NamuWikiRenderer.render(text).strip().splitlines():
+            if (not line
+                    or '.' not in line
+                    or utils.korean_character_ratio(line) < 0.1):
+                continue
+
+            # Remove duplicated spaces.
+            line = line.replace('\n', ' ').replace('\t', ' ')
+            while '  ' in line:
+                line = line.replace('  ', ' ')
+
+            # Normalize the quotes by replacing unusual ones to the standard
+            # ones.
+            line = NamuWikiParser.single_quotes_pattern.sub('\'', line)
+            line = NamuWikiParser.double_quotes_pattern.sub('"', line)
+
+            filtered.append(line)
+
+        return '\n'.join(filtered)
