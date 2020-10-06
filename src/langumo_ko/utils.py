7c16dcc54ddbb642b0f5e94fb9a55de41184e5fb
affjljoo3581 <affjljoo3581@gmail.com>
2020-10-06 23:08:20 +0900

    Add new utility function to test the character type

--- src/langumo_ko/utils.py
+++ src/langumo_ko/utils.py
@@ -7,3 +7,10 @@ def korean_character_ratio(text: str, ignore_whitespace: bool = True) -> float:
     korean_characters = len([
         c for c in text if ord('가') <= ord(c) <= ord('힣')])
     return korean_characters / len(text) if len(text) > 0 else 0
+
+
+def is_normal_character(c: str) -> bool:
+    return (ord('0') <= ord(c) <= ord('9')
+            or ord('a') <= ord(c) <= ord('z')
+            or ord('A') <= ord(c) <= ord('Z')
+            or ord('가') <= ord(c) <= ord('힣'))
